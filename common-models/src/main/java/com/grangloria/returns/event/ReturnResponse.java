7132aae21a7c8d6e5db0107b3f7363d1cf587d5f
James Hayes <james.hayes@granglorialabs.com>
2026-09-14 20:30:35 -0700

    refactor(common-models): remove shared library in favor of independent service contracts
    - Remove common-models Gradle module to eliminate tight coupling between microservices
    - Decentralize shared DTOs and event models into their respective service domains
    - Remove common-models from settings.gradle

--- common-models/src/main/java/com/grangloria/returns/event/ReturnResponse.java
+++
@@ -1,9 +0,0 @@
-package com.grangloria.returns.eventvent;
-
-// Response DTO for request acknowledgement
-public record ReturnResponse(
-        String trackingId,
-        String status,
-        String labelUrl,
-        Long timestamp
-) {}
